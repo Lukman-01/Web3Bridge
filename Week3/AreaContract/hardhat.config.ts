9776f875334d9b2aaddd93fee161a5bb1f5179f0
Lukman-01 <abdulyekeenlukman@gmail.com>
2024-08-22 20:30:57 +0100

    config

--- Week3/AreaContract/hardhat.config.ts
+++ Week3/AreaContract/hardhat.config.ts
@@ -1,8 +1,17 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+
+const ALCHEMY_HTTP_URL =  vars.get("ALCHEMY_HTTP_URL");
+const PRIVATE_KEY =  vars.get("PRIVATE_KEY");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: ALCHEMY_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
