d7d78725c9eeaa1659fa7908e6b6f47dd451b93c
Lukman-01 <abdulyekeenlukman@gmail.com>
2024-08-29 21:23:48 +0100

    deploy

---
+++ Week4/BankContracts/ignition/modules/deploy.ts
@@ -0,0 +1,15 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const DeployContracts = buildModule("DeployContracts", (m) => {
+  
+  // Deploy the first contract
+  const todoList = m.contract("TodoList");
+
+  // Deploy the second contract
+  const studentPortal = m.contract("StudentPortal");
+
+  // Return both contracts
+  return { todoList, studentPortal };
+});
+
+export default DeployContracts;
