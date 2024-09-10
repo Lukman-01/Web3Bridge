722024b8264ffe4874ec33de6f7c331a6e746f2d
Lukman-01 <abdulyekeenlukman@gmail.com>
2024-09-10 22:59:58 +0100

    .

--- Week5/MultiSig/hardhat.config.ts
+++ Week5/MultiSig/hardhat.config.ts
@@ -4,7 +4,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.26",
+  solidity: "0.8.24",
   networks: {
     // for testnet
     "lisk-sepolia": {
