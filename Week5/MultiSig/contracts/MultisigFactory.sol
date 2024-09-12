55b458cdb2c03784d161d50374f3ff6b7f904159
Lukman-01 <abdulyekeenlukman@gmail.com>
2024-09-12 23:46:10 +0100

    factory

---
+++ Week5/MultiSig/contracts/MultisigFactory.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "./MultiSig.sol";
+
+contract MultisigFactory{
+    MultiSig[] multisigClones;
+
+    function createMultisigWallet(uint8 _quorum, address[] memory _validSigners) external returns (MultiSig newMulsig_, uint256 length_) {
+
+        newMulsig_ = new MultiSig(_quorum, _validSigners);
+
+        multisigClones.push(newMulsig_);
+
+        length_ = multisigClones.length;
+    }
+
+    function getMultiSigClones() external view returns(MultiSig[] memory) {
+        return multisigClones;
+    }
+}
