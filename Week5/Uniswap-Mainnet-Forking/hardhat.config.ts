f27ca5faa65c445610f9a4ade476e619a6f21399
Lukman-01 <abdulyekeenlukman@gmail.com>
2024-09-12 23:54:27 +0100

    config

--- Week5/Uniswap-Mainnet-Forking/hardhat.config.ts
+++ Week5/Uniswap-Mainnet-Forking/hardhat.config.ts
@@ -1,8 +1,19 @@
-import { HardhatUserConfig } from "hardhat/config";
+// import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config({ path: ".env" });
 
-const config: HardhatUserConfig = {
+const ALCHEMY_MAINNET_API_KEY_URL = process.env.ALCHEMY_MAINNET_API_KEY_URL;
+const ACCOUNT_PRIVATE_KEY = process.env.ACCOUNT_PRIVATE_KEY;
+
+module.exports = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      forking: {
+        url: ALCHEMY_MAINNET_API_KEY_URL,
+      }
+    }
+  },
+  lockGasLimit: 200000000000,
+  gasPrice: 10000000000,
 };
-
-export default config;
