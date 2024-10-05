6ba2b85543defb306a75bc6391fac8528fcb7fc4
Lukman-01 <abdulyekeenlukman@gmail.com>
2024-10-05 11:20:53 +0100

    done

--- Week7/ERC20-Subgraph/erc20-subgraph/src/og-token.ts
+++ Week7/ERC20-Subgraph/erc20-subgraph/src/og-token.ts
@@ -1,66 +1,160 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
-  OGToken,
-  Approval,
   TokensBurned,
   TokensMinted,
-  Transfer
+  Transfer as TransferEvent,
+  Approval as ApprovalEvent,
 } from "../generated/OGToken/OGToken"
-import { ExampleEntity } from "../generated/schema"
+import {
+  Burn,
+  Approval,
+  Transfer,
+  User,
+  Token,
+  Mint
+} from "../generated/schema";
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from)
+export function handleApproval(event: ApprovalEvent): void {
+  let approval = new Approval(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+  approval.owner = event.params.owner;
+  approval.spender = event.params.spender;
+  approval.value = event.params.value;
+  approval.timestamp = event.block.timestamp;
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from)
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  let user = User.load(event.params.owner.toHex());
+  if (user == null) {
+    user = new User(event.params.owner.toHex());
+    user.balance = BigInt.fromI32(0);
+    user.token = event.address.toString();
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.allowance(...)
-  // - contract.approve(...)
-  // - contract.balanceOf(...)
-  // - contract.decimals(...)
-  // - contract.getBalanceOf(...)
-  // - contract.name(...)
-  // - contract.symbol(...)
-  // - contract.totalSupply(...)
-  // - contract.transfer(...)
-  // - contract.transferFrom(...)
+  approval.user = user.id;
+  approval.save();
 }
 
-export function handleTokensBurned(event: TokensBurned): void {}
+export function handleTokensBurned(event: TokensBurned): void {
+  // create a new Entity
+  let burn = new Burn(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
 
-export function handleTokensMinted(event: TokensMinted): void {}
+  burn.from = event.params.from;
+  burn.value = event.params.amount;
+  burn.timestamp = event.block.timestamp;
+  burn.user = event.params.from.toString();
+  burn.save();
 
-export function handleTransfer(event: Transfer): void {}
+  // update Sender balances
+  let fromUser = User.load(event.params.from.toHex());
+  if (fromUser == null) {
+    fromUser = new User(event.params.from.toHex());
+    fromUser.balance = BigInt.fromI32(0);
+    fromUser.token = event.address.toString();
+  }
+
+  fromUser.balance = fromUser.balance.minus(event.params.amount);
+  fromUser.save();
+
+  // update the total supply and total burned in tokens entity
+  let token = Token.load("Token Stats");
+  if (token === null) {
+    token = new Token("Token Stats");
+    token.totalSupply = BigInt.fromI32(0);
+    token.totalBurned = BigInt.fromI32(0);
+    token.totalMinted = BigInt.fromI32(0);
+    token.totalTranferred = BigInt.fromI32(0);
+  }
+
+  token.totalSupply = token.totalSupply.minus(event.params.amount);
+  token.totalBurned = token.totalBurned.plus(event.params.amount);
+  token.save();
+}
+
+export function handleTokensMinted(event: TokensMinted): void {
+  // create a new Entity
+  let mint = new Mint(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+
+  mint.to = event.params.to;
+  mint.value = event.params.amount;
+  mint.timestamp = event.block.timestamp;
+  mint.user = event.params.to.toString();
+  mint.save();
+
+  // update reciever balances
+  let toUser = User.load(event.params.to.toHex());
+  if (toUser == null) {
+    toUser = new User(event.params.to.toHex());
+    toUser.balance = BigInt.fromI32(0);
+    toUser.token = event.address.toString();
+  }
+
+  toUser.balance = toUser.balance.plus(event.params.amount);
+  toUser.save();
+
+  // update the total supply and total minted in tokens entity
+  let token = Token.load("Token Stats");
+  if (token === null) {
+    token = new Token("Token Stats");
+    token.totalSupply = BigInt.fromI32(0);
+    token.totalBurned = BigInt.fromI32(0);
+    token.totalMinted = BigInt.fromI32(0);
+    token.totalTranferred = BigInt.fromI32(0);
+  }
+
+  token.totalSupply = token.totalSupply.plus(event.params.amount);
+  token.totalMinted = token.totalMinted.plus(event.params.amount);
+  token.save();
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  // create a new Entity
+  let transfer = new Transfer(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+
+  transfer.from = event.params.from;
+  transfer.to = event.params.to;
+  transfer.value = event.params.value;
+  transfer.timestamp = event.block.timestamp;
+  transfer.user = event.params.from.toString();
+  transfer.save();
+
+  // update sender balances
+  let fromUser = User.load(event.params.from.toHex());
+  if (fromUser == null) {
+    fromUser = new User(event.params.from.toHex());
+    fromUser.balance = BigInt.fromI32(0);
+    fromUser.token = event.address.toString();
+  }
+
+  fromUser.balance = fromUser.balance.minus(event.params.value);
+  fromUser.save();
+
+  // update receiver balances
+  let toUser = User.load(event.params.to.toHex());
+  if (toUser == null) {
+    toUser = new User(event.params.to.toHex());
+    toUser.balance = BigInt.fromI32(0);
+    toUser.token = event.address.toString();
+  }
+
+  toUser.balance = toUser.balance.plus(event.params.value);
+  toUser.save();
+
+  // update the total transferred in tokens entity
+  let token = Token.load("Token Stats");
+  if (token === null) {
+    token = new Token("Token Stats");
+    token.totalSupply = BigInt.fromI32(0);
+    token.totalBurned = BigInt.fromI32(0);
+    token.totalMinted = BigInt.fromI32(0);
+    token.totalTranferred = BigInt.fromI32(0);
+  }
+
+  token.totalTranferred = token.totalTranferred.plus(event.params.value);
+  token.save();
+}
